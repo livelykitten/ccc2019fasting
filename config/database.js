c7931029c9383ac763a923ae3ce0b26268c1bbc9
Junyong Park <jyp0802@hotmail.com>
2018-12-20 14:17:18 +0900

    mysql pool

--- config/database.js
+++ config/database.js
@@ -6,5 +6,11 @@ module.exports = {
         'password': 'rootpassword'
     },
 	'database': '18fasting',
+    'poolconfig': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'rootpassword',
+        'database': '18fasting'
+    },
     'users_table': 'users'
 };
